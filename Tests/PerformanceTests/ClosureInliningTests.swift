2986b5e40adac4f25c126fa460d6df4b344cec02
araki <araki@qoncept.co.jp>
2017-08-24 17:38:20 +0900

    Add tests

--- Tests/PerformanceTests/ClosureInliningTests.swift
+++ Tests/PerformanceTests/ClosureInliningTests.swift
@@ -13,12 +13,32 @@ class ClosureInliningTests: XCTestCase {
         }
     }
     
-    func testNoinline() {
+    func testNoInline() {
         let x = (0..<1_000_000).map { Int($0) }
         
         measure {
             for _ in 0..<100 {
                 _ = x.noInlineMap { $0 + 1 }
+            }
+        }
+    }
+    
+    func testInlineCompound() {
+        var x = (0..<1_000_000).map { Int($0) }
+        
+        measure {
+            for _ in 0..<100 {
+                x.inlineMapCompound(+=, rhs: 1)
+            }
+        }
+    }
+    
+    func testNoInlineCompound() {
+        var x = (0..<1_000_000).map { Int($0) }
+        
+        measure {
+            for _ in 0..<100 {
+                x.noInlineMapCompound(+=, rhs: 1)
             }
         }
     }
@@ -41,5 +61,19 @@ extension Array where Element == Int {
             new[i] = f(new[i])
         }
         return new
+    }
+    
+    @inline(__always)
+    fileprivate mutating func inlineMapCompound(_ f: (inout Int, Int)->Void, rhs: Int) {
+        for i in 0..<count {
+            f(&self[i], rhs)
+        }
+    }
+    
+    @inline(never)
+    fileprivate mutating func noInlineMapCompound(_ f: (inout Int, Int)->Void, rhs: Int) {
+        for i in 0..<count {
+            f(&self[i], rhs)
+        }
     }
 }
